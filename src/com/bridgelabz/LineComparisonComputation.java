e7f4dd07ab028ab3518a63b6e98eddefa617bed8
drisyaathul <drisyak809@gmail.com>
2023-07-20 02:12:08 +0800

    Line Comparison Computation Program

---
+++ src/com/bridgelabz/LineComparisonComputation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class LineComparisonComputation {
+    public static void main(String[] args) {
+        System.out.println("*** Welcome To Line Comparison Computation Program ***");
+    }
+}
